31463c67ee8c7e7b649cf5b0d5eee2adaacdc921
yoounyoungheon <iddyoon@gmail.com>
2025-01-26 22:29:24 +0900

    [ui] form 컴포넌트 구현

---
+++ web/src/app/ui/molecule/form/form-index.tsx
@@ -0,0 +1,8 @@
+import { FormRoot } from "./form-root";
+import { FormSubmitButton } from "./form-submit-button";
+
+const Form = Object.assign(FormRoot, {
+  SubmitButtom: FormSubmitButton
+})
+
+export default Form;
