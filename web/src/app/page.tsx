b8807f55877da2cca82b41e9206ef92f9992f226
yoounyoungheon <iddyoon@gmail.com>
2025-01-26 23:05:06 +0900

    [ui] storybook 도입

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,10 @@
+import AchromaticButton from "./ui/atom/button/button";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/molecule/dialog/dialog";
+
 export default function Home() {
   return (
     <div>
-      
+
     </div>
   );
 }
